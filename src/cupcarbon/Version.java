315854a5db04434b963d6723d61fda56d529b806
bounceur <bounceur@gmail.com>
2015-11-01 17:16:51 +0100

    New Version 2.2.

--- src/cupcarbon/Version.java
+++ src/cupcarbon/Version.java
@@ -1,6 +1,6 @@
 package cupcarbon;
 
 public final class Version {
-	public static final String VERSION = "2.1 (U-One)";
-	public static final String YEAR = "October 2015";
+	public static final String VERSION = "2.2 (U-One)";
+	public static final String YEAR = "November 2015";
 }
