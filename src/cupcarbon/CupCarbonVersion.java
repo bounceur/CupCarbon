009748f78a1d412ed05c25c61dccd303a7ab1ea4
bounceur <bounceur@gmail.com>
2017-06-20 14:27:29 +0200

    New version.

--- src/cupcarbon/CupCarbonVersion.java
+++ src/cupcarbon/CupCarbonVersion.java
@@ -3,5 +3,5 @@ package cupcarbon;
 public final class CupCarbonVersion {
 	public static final String VERSION = "3.2.0 (U-One)";
 	public static final String YEAR = "2017";
-	public static final int UPDATE = 2;
+	public static final int UPDATE = 3;
 }
