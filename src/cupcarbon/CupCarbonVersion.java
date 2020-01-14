ca92810a66ddd5883c4f851a180af57382e12a1a
bounceur <bounceur@gmail.com>
2020-01-14 11:57:20 +0100

    Version 4.2

--- src/cupcarbon/CupCarbonVersion.java
+++ src/cupcarbon/CupCarbonVersion.java
@@ -4,5 +4,4 @@ public final class CupCarbonVersion {
 	public static final String VERSION = "U-One 4.2";
 	public static final String YEAR = "2020";
 	public static final int UPDATE = 25;
-	public static final int UPDATE2 = 25;
 }
