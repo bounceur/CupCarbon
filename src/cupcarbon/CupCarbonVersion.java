e3419af2de988a5351d6feb5441406fec6a93db6
bounceur <bounceur@mbp-de-bounceur>
2020-11-14 17:04:55 +0100

    New Version IoT (5.0)

--- src/cupcarbon/CupCarbonVersion.java
+++ src/cupcarbon/CupCarbonVersion.java
@@ -3,5 +3,5 @@ package cupcarbon;
 public final class CupCarbonVersion {
 	public static final String VERSION = "IoT 5.0";
 	public static final String YEAR = "2021";
-	public static final int UPDATE = 27;
+	public static final int UPDATE = 30;
 }
