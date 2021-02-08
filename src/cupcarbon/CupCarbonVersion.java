5e1f6bded54f692bdcd08a21cf2f3783412cef20
bounceur <bounceur@192.168.0.2>
2021-02-08 22:24:23 +0100

    Update.

--- src/cupcarbon/CupCarbonVersion.java
+++ src/cupcarbon/CupCarbonVersion.java
@@ -3,6 +3,5 @@ package cupcarbon;
 public final class CupCarbonVersion {
 	public static final String VERSION = "IoT 5.0";
 	public static final String YEAR = "2021";
-	public static final int UPDATE = 30;
-	public static final int tmp = 30;
+	public static final int UPDATE = 31;
 }
